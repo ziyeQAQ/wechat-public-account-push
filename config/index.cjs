7a01e9c5e74b398e94c8d5b60d15b123ae8cd020
ziyeQAQ <105693403+ziyeQAQ@users.noreply.github.com>
2025-04-01 13:50:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,56 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx520ffbad36548a38',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'e9822cd9556754e34c1efdea8b201dfd',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '四川',
+  CITY: '宜宾',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '巧巧',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ojdJP62CduzIgsYfuG_bve8DrALw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'LL8mbZdwu4DnwmDY9d6nZ7nsHHwxRUIE0_MYhasGfB4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '6-11',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '巧巧', year: '2006', date: '05-16',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '巧巧', year: '2006', date: '06-11',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2024', date: '08-02',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
+      // 我们在分开已经有xxxx天了的配置
       customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        // 分开的日子
+        { keyword: 'love_day', date: '2024-12-26' },
+        // 纪念日
+        { keyword: 'marry_day', date: '2025-08-02' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'LL8mbZdwu4DnwmDY9d6nZ7nsHHwxRUIE0_MYhasGfB4',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '紫夜',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ojdJP65KD3VyCTdMdGWGUBLrPM6U',
     }
   ],
 
